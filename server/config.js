d72dd34a6ab7826c5c214d3e5f131f4a27bd2b0a
Subodh Adhikari <subodh1adhikari@gmail.com>
2024-04-03 15:57:53 +0545

    commited after few changes

--- server/config.js
+++
@@ -1,1 +0,0 @@
-connectionString = `mongodb+srv://subodh1:UlVokdLS6zg8WuND@cluster0.yqn0k.mongodb.net/books-library?retryWrites=true&w=majority&appName=Cluster0`;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
-const Book = require('./models/bookModel');
+const Book = require("./models/bookModel");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -20,16 +20,17 @@ app.use(express.json());
 // connecting to MongoDB database
 mongoose
   .connect(process.env.MONGODB_STRING)
-  .then(console.log('Connected to Database'))
+  .then(console.log("Connected to Database"))
   .catch((err) => console.log(err));
 
 // http routes, two parameters: path, callback
-app.get('/', (req, res) => {
-  res.send('Hello from the express server');
+app.get("/", (req, res) => {
+  res.send("Hello from the express server");
 });
 
 // route to save new book
-app.post('/books', async (req, res) => {
+app.post("/books", async (req, res) => {
+  console.log("route to create new book");
   try {
     const newBook = {
       // to parse req.body we used middleware express.json()
@@ -45,7 +46,8 @@ app.post('/books', async (req, res) => {
 });
 
 // route to get all books from database
-app.get('/books', async (req, res) => {
+app.get("/books", async (req, res) => {
+  console.log("route to get all books");
   try {
     const books = await Book.find({});
     res.status(200).json(books);
@@ -55,10 +57,12 @@ app.get('/books', async (req, res) => {
 });
 
 // route to get book by specific id
-app.get('/books/:id', async (req, res) => {
+app.get("/books/:id", async (req, res) => {
+  console.log("route to get book by specific id");
   try {
+    console.log("I am inside get book by id route");
     const id = req.params.id;
-    console.log('id', id);
+    console.log("id", id);
     const book = await Book.findById(id);
     res.status(200).json(book);
   } catch (err) {
@@ -66,21 +70,26 @@ app.get('/books/:id', async (req, res) => {
   }
 });
 
-// route to delete a book by specific id
-app.delete('books/:id', async (request, response) => {
-  try {
-    const id = request.params.id;
+app.delete("/", (req, res) => {
+  console.log("I am inside home delete route");
+  res.send("DELETE Request Called");
+});
 
+// route to delete a book by specific id
+app.delete("/books/:id", async (req, res) => {
+  console.log("route to delete a book by id");
+  try {
+    const id = req.params.id;
     const result = await Book.findByIdAndDelete(id);
 
     if (!result) {
-      return response.status(404).json({ message: 'Book not found' });
+      return res.status(404).json({ message: "Book not found" });
     }
 
-    return response.status(204).send({ message: 'Book deleted successfully' });
+    return res.status(204).send({ message: "Book deleted successfully" });
   } catch (error) {
     console.log(error.message);
-    response.status(500).send({ message: error.message });
+    res.status(500).send({ message: error.message });
   }
 });
 
